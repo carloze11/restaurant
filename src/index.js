09ed844f5850b27557d5ac02f84271f6305a2eba
Carlos Estrada <cestrada1109@gmail.com>
2022-09-02 13:22:09 -0500

    Fix webpack discrepensies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+import _ from 'lodash';
 import { pageLoad } from "./pageLoad";
 import { header } from "./header";
 
+
 pageLoad();
 header();
+
+const tabs = document.querySelectorAll('.tab');
+tabs.forEach( tab => {
+    tab.addEventListener('click', () => {
+        if (tab === tabs[0]) {
+            alert('home!')
+        }
+        if (tab === tabs[1]) {
+            alert('menu!')
+        }
+        if (tab === tabs[2]) {
+            alert('contact!')
+        }
+    })
+})
+
+// homeTab.addEventListener('click', () => {
+//     alert('hello')
+//     pageLoad();
+//     header();
+//   });
+  
+//   menuTab.addEventListener('click', () => {
+//     loadMenu();
+//     header();
+
+//   });
+  
+//   contactTab.addEventListener('click', () => {
+//     loadContact();
+//     header();
+
+//   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        pageLoad: './src/pageload.js',
-        header: './src/header.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    }
-}
+  entry: {
+    index: './src/index.js',
+    pageLoad: './src/pageLoad.js',
+    header: './src/header.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Eevees Eateries',
+        template: 'template.html',
+    }),
+    new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
